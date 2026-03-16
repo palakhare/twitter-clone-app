c9f0672f6bdd17f38197db4b3a49caa82570c9d3
palak_khare1314 <palakkhare1310@gmail.com>
2026-03-16 10:51:43 +0530

    Update db.ts

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI not defined");
+}
 
 export default async function connectDB() {
   if (mongoose.connection.readyState === 1) return;
